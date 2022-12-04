9bd8304a59d809f07d4ca8af7ff18b896f634a26
Dmitriy <bezzubenko.dim@yandex.ru>
2022-12-04 17:22:27 +0700

    Task6-8

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string? numLine = Console.ReadLine();
+
+if(numLine != null)
+{
+    int num = int.Parse(numLine);
+    
+    if(num % 2 == 0)
+    {
+        Console.WriteLine("число является четным"); 
+    }
+    else 
+    {
+        Console.WriteLine("число является нечетным");
+    }
+
+}
+
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,30 @@
+﻿//вводим число
+string? numLine = Console.ReadLine();
+// проверяем ,чтобы данные не были пустыми 
+if(numLine != null)
+{
+    // Парсим число
+    int num = int.Parse(numLine);
+    // Ввел переменную для входа-выхода из цикла
+    int variableLine = 2 Console.ReadLine();
+        // Условия цикла
+        While(variable > num)
+        {
+
+        }
+         
+         if(variable % == 2)
+         
+         {
+            Console.WriteLine(variableLine + 1)
+         }
+
+    
+
+
+
+
+
+
+
+}
