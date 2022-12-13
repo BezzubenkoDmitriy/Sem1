f1a3dd94cdf23b87e451095c8a3a914cb30cab83
Dmitriy <bezzubenko.dim@yandex.ru>
2022-12-13 16:00:04 +0700

    add task 24,26,28,30

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+long CalcFactor(int num)
+{
+    long count = 1;
+    for(int i=1;i<=num;i++)
+    {
+        count *=i;
+    }
+    return count;
+}
+int number = ReadData("Введите число А");
+long lenght1 = CalcFactor(number);
+PrintData("Факториал равно"+lenght1);
