d76435c5d4ede107e103550ac80eb41d5c784fb8
Dmitriy <bezzubenko.dim@yandex.ru>
2022-12-13 21:21:00 +0700

    home work

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Запрос данных у пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+//Метод вывода результата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+// Метод возведения в степень
+int MPow(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+
+    return result;
+}
+
+int numberA = ReadData("Введите число А");
+int numberB = ReadData("Введите число B");
+int Pow = MPow(numberA, numberB);
+PrintData("Ответ: " + MPow);
+
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+
+
+//Запрос данных у пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+//Метод вывода результата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+//Метод суммы цифр
+  int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int numberN = ReadData("Введите число N:");
+int sumNumber = SumNumber(numberN);
+PrintData("Сумма цифр в числе: " + sumNumber);
