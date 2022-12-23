37ea95c87a5c39ac7739b44b9f3e96e73709232e
Dmitriy <bezzubenko.dim@yandex.ru>
2022-12-23 10:43:47 +0700

    office home work

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+int linesVol = Convert.ToInt32(Console.ReadLine());
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+ArrayRandomNumbers(numbers);
+PrintArray(numbers);
+//Метод рандомной генерации массива
+void ArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+//Метод вывода массива на консоле 
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
